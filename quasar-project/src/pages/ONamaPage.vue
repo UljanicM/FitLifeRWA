bbcf3f035a2a527903b13ea319da7d8780efb6f9
mlakovic02 <lakovicmarijan02@gmail.com>
2025-01-14 12:10:39 +0100

    Dodan email i broj mobitela u OnamaPage.vue

--- quasar-project/src/pages/ONamaPage.vue
+++ quasar-project/src/pages/ONamaPage.vue
@@ -1,13 +1,24 @@
 <template>
   <q-page class="flex flex-center">
     <q-card dark bordered class="bg-grey-9 my-card">
+      <!-- Slika i naslov -->
       <q-card-section>
         <q-img src="~assets/Fitlife1.jpg">
           <div class="text-h3 text-weight-bolder absolute-bottom text-center">O nama</div>
         </q-img>
       </q-card-section>
+
+      <!-- Opis -->
       <q-card-section class="text-h4 text-justify">
         {{ o_nama }}
+      </q-card-section>
+
+      <!-- Kontakt podaci -->
+      <q-card-section class="text-h5 q-mt-md">
+        <div class="text-center">
+          <p>Email: <a href="mailto:info@fitlife.com" class="text-primary">info@fitlife.com</a></p>
+          <p>Broj mobitela: <span class="text-primary">+385 99 123 4567</span></p>
+        </div>
       </q-card-section>
     </q-card>
   </q-page>
@@ -23,5 +34,9 @@ const o_nama = ref('Fitlife')
 <style scoped>
 .my-card {
   width: 100%;
+}
+.text-primary {
+  color: #1976d2;
+  font-weight: bold;
 }
 </style>
